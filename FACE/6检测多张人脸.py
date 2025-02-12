669490d3bc09abc381677986d675f2607e923498
Guilty <981196023@qq.com>
2025-02-12 21:05:04 +0800

    多张人脸识别

--- FACE/6检测多张人脸.py
+++ FACE/6检测多张人脸.py
@@ -1,4 +1,6 @@
 import cv2 as cv
+
+
 def face_detect_demo():
     # 转换为灰度图像
     gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -7,13 +9,14 @@ def face_detect_demo():
     face_detector = cv.CascadeClassifier(cv.data.haarcascades + 'haarcascade_frontalface_default.xml')
 
     # 检测人脸
-    faces = face_detector.detectMultiScale(gray,scaleFactor=1.3,minNeighbors=4)
+    faces = face_detector.detectMultiScale(gray, scaleFactor=1.3, minNeighbors=4)
     for x, y, w, h in faces:
-        print(x,y,w,h)
+        print(x, y, w, h)
         cv.rectangle(img, (x, y), (x + w, y + h), color=(0, 255, 255), thickness=2)
-        cv.circle(img,center=(x+w//2,y+h//2),radius=w//2,color=(0,255,255),thickness=2)
+        cv.circle(img, center=(x + w // 2, y + h // 2), radius=w // 2, color=(0, 255, 255), thickness=2)
     # 显示结果
     cv.imshow('result', img)
+
 
 img = cv.imread('k3.jpg')
 face_detect_demo()
