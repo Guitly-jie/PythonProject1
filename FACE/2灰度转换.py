562838ee6719c835d3058269ed66eedc4ae93731
Guilty <981196023@qq.com>
2025-02-12 19:04:12 +0800

    测验

--- FACE/2灰度转换.py
+++ FACE/2灰度转换.py
@@ -1,6 +1,15 @@
 import cv2 as cv
-img =cv.imread('01.jpg')
-gray_img =cv.cvColor
-cv.imshow('read_',img)
+img =cv.imread('k1.jpg')
+#灰度转换
+gray1_img =cv.cvtColor(img,cv.COLOR_RGB2BGR)
+gray2_img =cv.cvtColor(img,cv.COLOR_RGB2YCrCb)
+
+
+cv.imshow('gray1',gray1_img)
+cv.imshow('gray2',gray2_img)
+cv.imshow('read',img)
+
+cv.imwrite('gray1.jpg',gray1_img)
+cv.imwrite('gray2.jpg',gray2_img)
 cv.waitKey(0)
 cv.destroyAllWindows()
