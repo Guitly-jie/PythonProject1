14d3e0935c2a3a4c02852b1c0661b428384a2676
Guilty <981196023@qq.com>
2025-02-12 19:18:21 +0800

    测验

---
+++ FACE/3修改尺寸.py
@@ -0,0 +1,14 @@
+import cv2 as cv
+img=cv.imread('k1.jpg')
+resize_img=cv.resize(img,dsize=(2000,2400))
+
+cv.imshow('resize_img',resize_img)
+
+cv.imwrite('re.jpg',resize_img)
+
+print("原来图片的形状",img.shape)
+print("后来图片的形状",resize_img.shape)
+while True:
+    if ord("F")==cv.waitKey(0):
+        break
+cv.destroyAllWindows()
